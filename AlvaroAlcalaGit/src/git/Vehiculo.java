050888efe328ed15c932b5b36ee9e9642882e279
alumno <alumno@Lliurex-PC>
2017-05-18 11:15:26 +0200

    primera modificacion

---
+++ AlvaroAlcalaGit/src/git/Vehiculo.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Vehiculo {
+	private int numPuertas;
+	private int numRuedas;
+	
+	public Vehiculo(int numPuertas, int numRuedas) {
+		this.numPuertas = numPuertas;
+		this.numRuedas = numRuedas;
+	}
+}
